3153a9c133bf2129b023abb479c85bd9a6624ec5
benonybellamy <78795432+benony007@users.noreply.github.com>
2021-03-16 04:22:08 +0200

    Travail Perso

---
+++ index.js
@@ -0,0 +1,30 @@
+console.log('Welcome to 007');
+
+
+
+
+
+    
+
+-webkit-animation-name: changement_de_couleur;
+-webkit-animation-duration: 5s;
+-webkit-animation-timing-function: ease;
+-webkit-animation-delay: 4s;
+-webkit-animation-iteration-count: infinite;
+-webkit-animation-direction: alternate;
+
+
+-moz-animation-name: changement_de_couleur;
+-moz-animation-duration: 5s;
+-moz-animation-timing-function: ease;
+-moz-animation-delay: 4s;
+-moz-animation-iteration-count: infinite;
+-moz-animation-direction: alternate;
+
+
+-o-animation-name: changement_de_couleur;
+-o-animation-duration: 5s;
+-o-animation-timing-function: ease;
+-o-animation-delay: 4s;
+-o-animation-iteration-count: infinite;
+-o-animation-direction: alternate;
