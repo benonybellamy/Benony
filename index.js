8b1f0389ebb15acc157d3bb6372bd86994430b49
benonybellamy <78795432+benony007@users.noreply.github.com>
2021-03-19 23:18:35 +0200

    Fin HTML & CSS

--- index.js
+++ index.js
@@ -1,30 +1,89 @@
-console.log('Welcome to 007');
+let five = 5;
 
+let two = 2;
 
+let nine;
 
+let someString = "Hello world!!!!";
 
+two = 4;
+two = 14;
+two = 22;
+two = 76;
+two = five;
+someString = five;
 
-    
+console.log("our variables", five, two);
+console.log(someString);
+console.log(nine);
 
--webkit-animation-name: changement_de_couleur;
--webkit-animation-duration: 5s;
--webkit-animation-timing-function: ease;
--webkit-animation-delay: 4s;
--webkit-animation-iteration-count: infinite;
--webkit-animation-direction: alternate;
+nine = 9;
 
+console.log(nine);
 
--moz-animation-name: changement_de_couleur;
--moz-animation-duration: 5s;
--moz-animation-timing-function: ease;
--moz-animation-delay: 4s;
--moz-animation-iteration-count: infinite;
--moz-animation-direction: alternate;
+const PI = 3.14;
 
+console.log(PI);
+console.log("-----------------------------");
 
--o-animation-name: changement_de_couleur;
--o-animation-duration: 5s;
--o-animation-timing-function: ease;
--o-animation-delay: 4s;
--o-animation-iteration-count: infinite;
--o-animation-direction: alternate;
+//number
+let num1 = 6;
+let num2 = 613242525235;
+let num3 = -2525235;
+let num4 = 0;
+
+console.log(num1, num2, num3, num4);
+console.log(typeof num2);
+console.log("-----------------------------");
+
+//string
+let str1 = "lalalala";
+let str2 = "qweqe qw eq weq we qe q weq ew qwe";
+let str3 = "H";
+let str4 = "";
+
+console.log(str1, str2, str3, str4);
+console.log(typeof str4);
+console.log("-----------------------------");
+
+//boolean
+let trueVariable = true; //правда
+let falseVariable = false; //ложь
+
+console.log(trueVariable, falseVariable);
+console.log(typeof trueVariable);
+console.log("-----------------------------");
+
+//undefined
+let undefinedValue = undefined; //
+
+console.log(undefinedValue);
+console.log(typeof undefinedValue);
+console.log("-----------------------------");
+
+//null
+let nullValue = null; //
+
+console.log(nullValue);
+console.log(typeof nullValue); // epic fail (return object)
+console.log("-----------------------------");
+
+//bigInt
+let bigIntValue = 9999999999999999999999999999999999999999999999999999999999999999999999999999n; //
+
+console.log(bigIntValue);
+console.log(typeof bigIntValue); // epic fail (return object)
+console.log("-----------------------------");
+
+//Symbol
+
+//object
+let obj = {
+  name: "Andrey",
+  isAdmin: true,
+  age: 26,
+}; //
+
+console.log(obj);
+console.log(typeof obj); // epic fail (return object)
+console.log("-----------------------------");
