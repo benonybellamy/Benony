06a6ea876fcdf105858cedd2f89af74185472143
benonybellamy <78795432+benony007@users.noreply.github.com>
2021-04-01 19:17:37 +0300

    changes

--- HOMEWORK/homeworks.js
+++ HOMEWORK/homeworks.js
@@ -316,18 +316,3 @@
 
 
 
-// Exercise 6: Output prime numbers
-
-
-
-let n = 10;
-
-label:
-for (let i = 2; i < 10; i++) { 
-
-  for (let j = 2; j < i; j++) {
-    if (i % j == 0) continue label; 
-  }
-
-  alert( i ); 
-}
