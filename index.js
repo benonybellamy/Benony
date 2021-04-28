51d87ceb3946bd836601b010bcf32768d0fe3c87
benonybellamy <78795432+benony007@users.noreply.github.com>
2021-04-28 20:45:02 +0300

    changes

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-// Exercise 6: Output prime numbers
 
 
 
-let n = 10;
-
-label:
-for (let i = 2; i < 10; i++) { 
-
-  for (let j = 2; j < i; j++) {
-    if (i % j == 0) continue label; 
+function min(a, b) {
+    if (a < b) {
+      return a;
+    } else {
+      return b;
+    }
   }
-
-  alert( i ); 
-}
